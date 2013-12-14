096d29a8adba2c8b3c8e1a835b2cb06c9a70afa9
shaul hameed <shaul21h@gmail.com>
2013-12-14 15:52:53 +0530

    Added Styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,6 +33,24 @@ module.exports =function(grunt){
 				}]
 			}
 		},
+		less:{
+			base:{
+				options: {
+					minified: true
+				},
+				files:{
+					'_public/styles/boot.css':'vendor/bootstrap/bootstrap.less'
+				}
+			},
+			all:{
+				options: {
+					minified: false
+				},
+				files:[
+					{dest:'_public/styles', src:'*.less',cwd:'less/', ext:'.css', expand:true }
+				]
+			}
+		},
 		watch:{
 			jade:{
 				files: ['**/*.jade'],
@@ -40,13 +58,23 @@ module.exports =function(grunt){
 				options: {
 					livereload: true
 				}
-			}		
+			},
+			less:{
+				files: ["less/*.less"],
+				tasks: ['less:all'],
+				options: {
+					livereload: true
+				}
+			}
+
 		}
 
 	});
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-jade');
 	grunt.loadNpmTasks('grunt-express');
+	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default', ['jade','express','watch']);
+	grunt.registerTask('default', ['jade','express','less:all','watch']);
+	grunt.registerTask('compileless', ['less:base']);
 }
